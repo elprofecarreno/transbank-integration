c3c3cf2919407ee8d05b00e4a492b987e09bb0d3
Cristian Carreño <elprofecarreno@gmail.com>
2023-06-09 17:19:34 -0400

    add flask portal web

---
+++ portal_flask_web/manage.py
@@ -0,0 +1,32 @@
+# IMPORT FLASK LIB
+from flask import Flask
+# IMPORT LIB OPERATION SYSTEM
+import os
+# IMPORT LOAD ENVIRONMENT (.env)
+from dotenv import load_dotenv
+# LOAD ENVIRONMENT
+load_dotenv()
+# IMPORT VIEWS
+from views.view_index import app_transbank_pay_view
+
+# LOAD PATH PARENTS FOLDER
+BASE_DIR = os.path.abspath(os.getcwd())
+print('BASE_DIR: ', BASE_DIR)
+
+# CREATE PATH TEMPLATES FOLDER
+TEMPLATE_FOLDER = os.path.join(BASE_DIR, 'templates')
+print('TEMPLATE_FOLDER: ', TEMPLATE_FOLDER)
+
+# LOAD TEMPLATES
+STATIC_FOLDER = os.path.join(BASE_DIR, 'static')
+print('STATIC_FOLDER: ', STATIC_FOLDER)
+
+# CREATE FLASK APP WEB
+app = Flask(__name__, template_folder=TEMPLATE_FOLDER, static_folder=STATIC_FOLDER)
+
+# LOAD VIEW
+app_transbank_pay_view(app)
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('APP_WEB_HOST'), port=os.getenv('APP_WEB_PORT'), debug=True)
+
---
+++ portal_flask_web/views/view_index.py
@@ -0,0 +1,52 @@
+# IMPORT FLASK LIB
+from flask import render_template
+# IMPORT FLASK LIB REQUEST OBJECT
+from flask import request
+# IMPORT LIBRERIES ERROR TRACE, OPERATION SYSTEM AND REQUEST CLIENT API REST
+import traceback, os, requests
+
+def app_transbank_pay_view(app):
+
+    API_REST_HOST = os.getenv('API_REST_HOST')
+    API_REST_PORT = os.getenv('API_REST_PORT')
+
+    @app.route('/transbank-pay', methods=['GET', 'POST'])
+    def transbank_pay_view():
+        print('METHOD', request.method)
+
+        if request.method == 'GET':
+            buy_order = '10029393040405'
+            amount = 250000
+            context = {
+                'buy_order': buy_order,
+                'amount': amount
+            }
+            return render_template('transbank-pay.html', context=context)
+        elif  request.method == 'POST':
+            buy_order = request.form.get('buy-order')
+            session_id = 12345786 # SESSION ID USER
+            amount = request.form.get('amount')
+            return_url = 'http://127.0.0.1:9000/commit-pay'
+
+            body = {
+                "buy_order": buy_order,
+                "session_id": session_id,
+                "amount": amount,
+                "return_url": return_url
+                }
+            host = os.getenv('API_REST_HOST')
+            port = os.getenv('API_REST_PORT')
+                             
+            url = 'http://{0}:{1}/api/v1/transbank/transaction/create'.format(host, port)
+            print('url: ', url)
+            response = requests.post(url, json=body)
+            json_response = response.json()
+            print('json_response: ', json_response)
+            if response.status_code == 200 :
+                context = {
+                    'amount': amount,
+                    'transbank' : json_response
+                }
+                return render_template('send-pay.html', context=context)
+            else: 
+                return render_template('ERROR TRANSBANK CREATE TRANSACTION')
